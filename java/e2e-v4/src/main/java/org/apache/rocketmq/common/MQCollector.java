412a8e5950b62eb135bfcafcf8b8bd69f7414807
cryptoya <cryptohttp@gmail.com>
2023-01-09 19:23:52 +0800

    add e2e cases of rocketmq4.x with new module

---
+++ java/e2e-v4/src/main/java/org/apache/rocketmq/common/MQCollector.java
@@ -0,0 +1,121 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.rocketmq.common;
+
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.utils.RandomUtils;
+import org.apache.rocketmq.utils.data.collect.DataCollector;
+import org.apache.rocketmq.utils.data.collect.DataCollectorManager;
+
+public abstract class MQCollector {
+    /**
+     * 成功入队的消息（发送成功）
+     */
+    protected DataCollector<MessageExt> enqueueMessages = null;
+    protected DataCollector<MessageExt> enqueueFailedMessages = null;
+    /**
+     * 成功出队的消息（消费返回成功）
+     */
+    protected DataCollector<MessageExt> dequeueMessages = null;
+    /**
+     * 全部出队的消息（包含重试消息）
+     */
+    protected DataCollector<MessageExt> dequeueAllMessages = null;
+    /**
+     * 全部出队的消息体（去重）
+     */
+    protected DataCollector<String> dequeueUndupMessageBody = null;
+    //消息发送响应时间收集器
+    protected DataCollector msgRTs = null;
+
+    public MQCollector() {
+        enqueueMessages = DataCollectorManager.getInstance().fetchListDataCollector(RandomUtils.getStringByUUID());
+        enqueueFailedMessages = DataCollectorManager.getInstance().fetchListDataCollector(RandomUtils.getStringByUUID());
+        dequeueMessages = DataCollectorManager.getInstance().fetchListDataCollector(RandomUtils.getStringByUUID());
+        dequeueAllMessages = DataCollectorManager.getInstance().fetchListDataCollector(RandomUtils.getStringByUUID());
+        dequeueUndupMessageBody = DataCollectorManager.getInstance().fetchListDataCollector(RandomUtils.getStringByUUID());
+
+        msgRTs = DataCollectorManager.getInstance().fetchListDataCollector(RandomUtils.getStringByUUID());
+    }
+
+    public void clearMsg() {
+        enqueueMessages.resetData();
+        enqueueFailedMessages.resetData();
+        dequeueMessages.resetData();
+        dequeueAllMessages.resetData();
+        dequeueUndupMessageBody.resetData();
+        msgRTs.resetData();
+    }
+
+    public void lockCollectors() {
+        enqueueMessages.lockIncrement();
+        enqueueFailedMessages.lockIncrement();
+        dequeueMessages.lockIncrement();
+        dequeueAllMessages.lockIncrement();
+        dequeueUndupMessageBody.lockIncrement();
+        msgRTs.lockIncrement();
+    }
+
+    public DataCollector<MessageExt> getEnqueueMessages() {
+        return enqueueMessages;
+    }
+
+    public void setEnqueueMessages(DataCollector<MessageExt> enqueueMessages) {
+        this.enqueueMessages = enqueueMessages;
+    }
+
+    public DataCollector<MessageExt> getEnqueueFailedMessages() {
+        return enqueueFailedMessages;
+    }
+
+    public void setEnqueueFailedMessages(DataCollector<MessageExt> enqueueFailedMessages) {
+        this.enqueueFailedMessages = enqueueFailedMessages;
+    }
+
+    public DataCollector<MessageExt> getDequeueMessages() {
+        return dequeueMessages;
+    }
+
+    public void setDequeueMessages(DataCollector<MessageExt> dequeueMessages) {
+        this.dequeueMessages = dequeueMessages;
+    }
+
+    public DataCollector<MessageExt> getDequeueAllMessages() {
+        return dequeueAllMessages;
+    }
+
+    public void setDequeueAllMessages(DataCollector<MessageExt> dequeueAllMessages) {
+        this.dequeueAllMessages = dequeueAllMessages;
+    }
+
+    public DataCollector<String> getDequeueUndupMessageBody() {
+        return dequeueUndupMessageBody;
+    }
+
+    public void setDequeueUndupMessageBody(DataCollector<String> dequeueUndupMessageBody) {
+        this.dequeueUndupMessageBody = dequeueUndupMessageBody;
+    }
+
+    public DataCollector getMsgRTs() {
+        return msgRTs;
+    }
+
+    public void setMsgRTs(DataCollector msgRTs) {
+        this.msgRTs = msgRTs;
+    }
+}
---
+++ java/e2e-v4/src/main/java/org/apache/rocketmq/frame/ResourceInit.java
@@ -0,0 +1,186 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.rocketmq.frame;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.rocketmq.account.Account;
+import org.apache.rocketmq.utils.MQAdmin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class ResourceInit {
+    private static final Logger log = LoggerFactory.getLogger(ResourceInit.class);
+
+    /**
+     * Read the configuration file to use
+     */
+    public static SystemConfigFileHelper configFileHelper = new SystemConfigFileHelper();
+    public static Account account = null;
+    /**
+     * Access point Information
+     */
+    protected static String namesrvAddr = null;
+    protected static Boolean aclEnable = null;
+    protected static String allIp;
+    protected static String cluster;
+    protected static List<String> nameserverIpList = new ArrayList<>();
+    protected static List<String> brokerIpList = new ArrayList<>();
+    protected static String nameserverPort = "9876";
+    private static Properties properties = null;
+
+    static {
+        initResource();
+        MQAdmin.initMQAdminExtInstance(namesrvAddr);
+        log.info("Init process success");
+    }
+
+    private static void initResource() {
+        try {
+            String env = System.getProperty("env", "daily");
+            String region = System.getProperty("region", "daily");
+            InputStream inputStream = new FileInputStream(String.format("src/test/resources/env/%s/%s.conf", env, region));
+            log.info("INIT - use config env:{}, config:{}", env, region);
+            properties = configFileHelper.loadConfig(inputStream);
+        } catch (Exception e) {
+            log.error(ExceptionUtils.getStackTrace(e));
+            log.info("INIT - execute error,exit the process");
+            System.exit(0);
+        }
+        // init endpoint and nameserver
+        initConnectionInfo();
+        initAcl();
+
+        String namesrvAddrIp = namesrvAddr.substring(0, namesrvAddr.indexOf(":"));
+        String namesrvAddrPort = namesrvAddr.substring(namesrvAddr.lastIndexOf(":") + 1);
+        checkConnection(namesrvAddrIp, namesrvAddrPort);
+
+    }
+
+    private static void initAcl() {
+        aclEnable = Boolean.parseBoolean(System.getProperty("aclEnable", properties.getProperty("aclEnable", "false")));
+        if (aclEnable) {
+            String accessKey = System.getProperty("accessKey", properties.getProperty("accessKey"));
+            String secretKey = System.getProperty("secretKey", properties.getProperty("secretKey"));
+            account = new Account(accessKey, secretKey);
+            log.info("INIT - acl is enabled, [accessKey:{}, secretKey:{}]", accessKey, secretKey);
+        } else {
+            log.info("INIT - acl is disabled");
+        }
+    }
+
+    private static void initConnectionInfo() {
+        allIp = System.getProperty("ALL_IP", properties.getProperty("ALL_IP"));
+        if (allIp != null) {
+            String[] allPodInfos = allIp.split(",");
+            for (String podInfo : allPodInfos) {
+                if (podInfo.contains("nameserver")) {
+                    nameserverIpList.add(podInfo.substring(podInfo.indexOf(":") + 1));
+                } else if (podInfo.contains("broker")) {
+                    brokerIpList.add(podInfo.substring(podInfo.indexOf(":") + 1));
+                }
+            }
+            if (nameserverIpList.isEmpty()) {
+                log.warn("INIT- Get nameserver from external is empty");
+                namesrvAddr = System.getProperty("namesrvAddr", properties.getProperty("namesrvAddr"));
+            } else {
+                namesrvAddr = nameserverIpList.get(0) + ":" + nameserverPort;
+            }
+        } else {
+            log.info("INIT- Get ALL_IP is null, use local info");
+            namesrvAddr = System.getProperty("namesrvAddr", properties.getProperty("namesrvAddr"));
+        }
+        cluster = System.getProperty("cluster", properties.getProperty("cluster"));
+        if (cluster == null) {
+            log.error("INIT- cluster is null, system exit");
+            System.exit(-1);
+        }
+        log.info("INIT- namesrvAddr:{}, cluster:{}", namesrvAddr, cluster);
+    }
+
+    private static void checkConnection(String ip, String port) {
+        boolean isConnected = telnet(ip, Integer.parseInt(port), 5000);
+        if (!isConnected) {
+            log.error("INIT - check: telnet {} {} ==> timeout: {}, isConnected: false", ip, port, 5000);
+            log.error("INIT - Warning, exit!!!");
+            System.exit(0);
+        } else {
+            log.info("INIT - check: telnet {} {} ==> isConnected: true", ip, port);
+        }
+    }
+
+    public static class SystemConfigFileHelper {
+        private String file;
+
+        public SystemConfigFileHelper() {
+        }
+
+        public Properties loadConfig() throws Exception {
+            InputStream in = new BufferedInputStream(new FileInputStream(file));
+            Properties properties = new Properties();
+            properties.load(in);
+            in.close();
+            return properties;
+        }
+
+        public Properties loadConfig(InputStream inputStream) throws Exception {
+            InputStream in = new BufferedInputStream(inputStream);
+            Properties properties = new Properties();
+            properties.load(in);
+            in.close();
+            return properties;
+        }
+
+        public void update(Properties properties) throws Exception {
+            log.error("[SystemConfigFileHelper] update no thing.");
+        }
+
+        public void setFile(String file) {
+            this.file = file;
+        }
+
+        public String getFile() {
+            return file;
+        }
+    }
+
+    public static boolean telnet(String hostname, int port, int timeout) {
+        Socket socket = new Socket();
+        boolean isConnected = false;
+        try {
+            socket.connect(new InetSocketAddress(hostname, port), timeout);
+            isConnected = socket.isConnected();
+        } catch (IOException e) {
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+            }
+        }
+        return isConnected;
+    }
+}
