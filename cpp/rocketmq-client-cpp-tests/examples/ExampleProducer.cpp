e38cbf570ba05bf93772018c4c5074fad41d93e0
Zhang Weiwei <1403767615@qq.com>
2023-07-04 23:05:41 +0800

    更新格式

--- cpp/rocketmq-client-cpp-tests/examples/ExampleProducer.cpp
+++ cpp/rocketmq-client-cpp-tests/examples/ExampleProducer.cpp
@@ -15,114 +15,50 @@
  * limitations under the License.
  */
 #include <algorithm>
-#include <atomic>
 #include <iostream>
 #include <memory>
-#include <random>
 #include <string>
 #include <system_error>
 
-#include "gflags/gflags.h"
-#include "rocketmq/CredentialsProvider.h"
+#include "rocketmq/Configuration.h"
 #include "rocketmq/Logger.h"
 #include "rocketmq/Message.h"
 #include "rocketmq/Producer.h"
+#include "rocketmq/SendReceipt.h"
 
-using namespace ROCKETMQ_NAMESPACE;
+std::string topic = "TopicTest";
+std::string access_point = "127.0.0.1:9876";
+int message_body_size = 4096;
 
-const std::string& alphaNumeric() {
-  static std::string alpha_numeric("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-  return alpha_numeric;
-}
+int main() {
 
-std::string randomString(std::string::size_type len) {
-  std::string result;
-  result.reserve(len);
-  std::random_device rd;
-  std::mt19937 generator(rd());
-  std::string source(alphaNumeric());
-  std::string::size_type generated = 0;
-  while (generated < len) {
-    std::shuffle(source.begin(), source.end(), generator);
-    std::string::size_type delta = std::min({len - generated, source.length()});
-    result.append(source.substr(0, delta));
-    generated += delta;
-  }
-  return result;
-}
+  auto configuration = ROCKETMQ_NAMESPACE::Configuration::newBuilder().withEndpoints(access_point).build();
 
-DEFINE_string(topic, "standard_topic_sample", "Topic to which messages are published");
-DEFINE_string(access_point, "127.0.0.1:9876", "Service access URL, provided by your service provider");
-DEFINE_int32(message_body_size, 4096, "Message body size");
-DEFINE_uint32(total, 256, "Number of sample messages to publish");
-DEFINE_string(access_key, "", "Your access key ID");
-DEFINE_string(access_secret, "", "Your access secret");
+  auto producer = ROCKETMQ_NAMESPACE::Producer::newBuilder().withConfiguration(configuration).build();
 
-int main(int argc, char* argv[]) {
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
-
-  auto& logger = getLogger();
-  logger.setConsoleLevel(Level::Info);
-  logger.setLevel(Level::Info);
-  logger.init();
-
-  // Access Key/Secret pair may be acquired from management console
-  CredentialsProviderPtr credentials_provider;
-  if (!FLAGS_access_key.empty() && !FLAGS_access_secret.empty()) {
-    credentials_provider = std::make_shared<StaticCredentialsProvider>(FLAGS_access_key, FLAGS_access_secret);
-  }
-
-  // In most case, you don't need to create too many producers, singletion pattern is recommended.
-  auto producer = Producer::newBuilder()
-                      .withConfiguration(Configuration::newBuilder()
-                                             .withEndpoints(FLAGS_access_point)
-                                             .withCredentialsProvider(credentials_provider)
-                                             .build())
-                      .build();
-
-  std::atomic_bool stopped;
-  std::atomic_long count(0);
-
-  auto stats_lambda = [&] {
-    while (!stopped.load(std::memory_order_relaxed)) {
-      long cnt = count.load(std::memory_order_relaxed);
-      while (count.compare_exchange_weak(cnt, 0)) {
-        break;
-      }
-      std::this_thread::sleep_for(std::chrono::seconds(1));
-      std::cout << "QPS: " << cnt << std::endl;
-    }
-  };
-
-  std::thread stats_thread(stats_lambda);
-
-  std::string body = randomString(FLAGS_message_body_size);
+  std::string body = "123";
 
   try {
-    for (std::size_t i = 0; i < FLAGS_total; ++i) {
-      auto message = Message::newBuilder()
-                         .withTopic(FLAGS_topic)
+    std::cout<<"hh"<<std::endl;
+    for (std::size_t i = 0; i < 5; ++i) {
+      auto message = ROCKETMQ_NAMESPACE::Message::newBuilder()
+                         .withTopic(topic)
                          .withTag("TagA")
-                         .withKeys({"Key-" + std::to_string(i)})
                          .withBody(body)
                          .build();
       std::error_code ec;
-      SendReceipt send_receipt = producer.send(std::move(message), ec);
+      ROCKETMQ_NAMESPACE::SendReceipt send_receipt = producer.send(std::move(message), ec);
       if (ec) {
-        std::cerr << "Failed to publish message to " << FLAGS_topic << ". Cause: " << ec.message() << std::endl;
+        std::cerr << "Failed to publish message to " << topic << ". Cause: " << ec.message() << std::endl;
       } else {
-        std::cout << "Publish message to " << FLAGS_topic << " OK. Message-ID: " << send_receipt.message_id
+        std::cout << "Publish message to " << topic << " OK. Message-ID: " << send_receipt.message_id
                   << std::endl;
-        count++;
       }
     }
   } catch (...) {
-    std::cerr << "Ah...No!!!" << std::endl;
+    std::cerr << "ERROR" << std::endl;
   }
-  stopped.store(true, std::memory_order_relaxed);
-  if (stats_thread.joinable()) {
-    stats_thread.join();
-  }
-
+  
+  std::cout<<"helloworld"<<std::endl;
   return EXIT_SUCCESS;
 }
